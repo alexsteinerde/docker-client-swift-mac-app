cb429c90ea28909e4239a3d1b3139808a4408520
Alexander Steiner <info@alexsteiner.de>
2021-04-06 06:59:51 +0200

    Shutdown docker client manually on app termination

--- DockerClientMacApp/AppDelegate.swift
+++ DockerClientMacApp/AppDelegate.swift
@@ -41,6 +41,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        try! dockerClient.syncShutdown()
     }
 }
 
